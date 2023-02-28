0e38192878089e26916b1eef2f2cc9d46ef4892d
thibaud-heub <thibaud.heub@gmail.com>
2023-02-28 17:12:48 +0100

    Add eslint and lint TD1.js

--- TD1.js
+++ TD1.js
@@ -1,19 +1,20 @@
-const fs = require('fs');
-let rawdata = fs.readFileSync('users.json');
-let users = JSON.parse(rawdata);
+const fs = require("fs");
+const process = require("process");
+const rawdata = fs.readFileSync("users.json");
+const users = JSON.parse(rawdata);
 
-let attribute = process.argv[2];
-let orderedList = [];
-let orderedListIndex = undefined;
+const attribute = process.argv[2];
+const orderedList = [];
+let orderedListIndex;
 
 for (let i = 0; i < users.length; i++) {
-    orderedListIndex = orderedList.findIndex((element) => element[attribute] == users[i][attribute]);
-    if (orderedListIndex == -1) {
-        orderedList.push({ [attribute]: users[i][attribute], "counter": 1 });
+    orderedListIndex = orderedList.findIndex((element) => element[attribute] === users[i][attribute]);
+    if (orderedListIndex === -1) {
+        orderedList.push({[attribute]: users[i][attribute], counter: 1});
     } else {
-        orderedList[orderedListIndex]["counter"] += 1;
+        orderedList[orderedListIndex].counter += 1;
     }
 }
 
-orderedList.sort((a,b) => (a["counter"] <= b["counter"])?1:-1)
+orderedList.sort((a, b) => b.counter - a.counter);
 console.log(orderedList);
