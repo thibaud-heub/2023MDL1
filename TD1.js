397dd36742a1700d7ac25bb1374e8eabbf6a6edc
thibaud-heub <thibaud.heub@gmail.com>
2023-02-28 17:37:04 +0100

    Split code in functions

--- TD1.js
+++ TD1.js
@@ -1,20 +1,32 @@
 const fs = require("fs");
 const process = require("process");
-const rawdata = fs.readFileSync("users.json");
-const users = JSON.parse(rawdata);
 
-const attribute = process.argv[2];
-const orderedList = [];
-let orderedListIndex;
+function createCounterList(users, attribute) {
+    const counterList = [];
+    let counterListIndex;
 
-for (let i = 0; i < users.length; i++) {
-    orderedListIndex = orderedList.findIndex((element) => element[attribute] === users[i][attribute]);
-    if (orderedListIndex === -1) {
-        orderedList.push({[attribute]: users[i][attribute], counter: 1});
-    } else {
-        orderedList[orderedListIndex].counter += 1;
+    for (let i = 0; i < users.length; i++) {
+        counterListIndex = counterList.findIndex((element) => element[attribute] === users[i][attribute]);
+        if (counterListIndex === -1) {
+            counterList.push({ [attribute]: users[i][attribute], counter: 1 });
+        } else {
+            counterList[counterListIndex].counter += 1;
+        }
     }
+
+    return counterList;
 }
 
-orderedList.sort((a, b) => b.counter - a.counter);
-console.log(orderedList);
+function main() {
+    const rawdata = fs.readFileSync("users.json");
+    const users = JSON.parse(rawdata);
+
+    const attribute = process.argv[2];
+
+    let counterList = createCounterList(users, attribute);
+
+    counterList.sort((a, b) => b.counter - a.counter);
+    console.log(counterList);
+}
+
+main();
