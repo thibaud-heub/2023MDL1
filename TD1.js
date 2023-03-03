ea131890df58bc9b55650689537380e7c55bfbf3
thibaud-heub <thibaud.heub@gmail.com>
2023-03-03 12:10:03 +0100

    Split code in more functions

--- TD1.js
+++ TD1.js
@@ -6,37 +6,16 @@ const inquirer = require("inquirer");
 function getUsers() {
     const rawdata = fs.readFileSync("users.json");
     const users = JSON.parse(rawdata);
-    return users
+    return users;
 }
 
-// Create a list with the number of occurrences of each attribute value in users
-function createCountList(users, attribute) {
-    const countList = [];
-    let countListIndex;
-
-    for (const user of users) {
-        countListIndex = countList.findIndex((element) => element[attribute] === user[attribute]);
-        if (countListIndex === -1) {
-            countList.push({ [attribute]: user[attribute], count: 1 }); // Push if not already in the list
-        } else {
-            countList[countListIndex].count += 1; // Increase counter if already in the list
-        }
-    }
-
-    return countList;
-}
-
-async function main() {
-
-    // Load content of users.json
-    const users = getUsers();
+// Display menu
+async function menu(users) {
 
     // Loop until user wants to quit
     let done = false;
     // eslint-disable-next-line no-constant-condition
     while (!done) {
-
-        // Display menu
         await inquirer.prompt({
             type: "list",
             name: "attribute",
@@ -52,17 +31,38 @@ async function main() {
                 done = true;
                 return;
             }
-
-            // Create the counterList of the selected attribute
             const countList = createCountList(users, answers.attribute);
-
-            // Sort in descending order the list
-            countList.sort((a, b) => b.count - a.count);
-
-            // Display the list as a table
-            console.table(countList);
+            displayCountList(countList);
         });
     }
+}
+
+// Create a list with the number of occurrences of each attribute value in users
+function createCountList(users, attribute) {
+    const countList = [];
+    let countListIndex;
+
+    for (const user of users) {
+        countListIndex = countList.findIndex((element) => element[attribute] === user[attribute]);
+        if (countListIndex === -1) {
+            countList.push({ [attribute]: user[attribute], count: 1 }); // Push if not already in the list
+        } else {
+            countList[countListIndex].count += 1; // Increase counter if already in the list
+        }
+    }
+    countList.sort((a, b) => b.count - a.count);
+
+    return countList;
+}
+
+// Display countList as a table
+function displayCountList(countList) {
+    console.table(countList);
+}
+
+function main() {
+    const users = getUsers();
+    menu(users);
 }
 
 main();
